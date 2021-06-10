5654ee0577fe908aa7241577291afdf68e2eb850
i am jake <goldinghaml@stu.otc.school.nz>
2021-06-11 10:37:35 +1200

    initial commit

---
+++ Assets/Mouse/MouseMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    [Header("Mouse")]
+    public GameObject MouseSprite;
+    public ClickLogic a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseSprite.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition)+new Vector3(0f, 0f, 1f);
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null) {
+                string objectHit = hit.collider.ToString();
+                objectHit = objectHit.Split(char.Parse(" "))[0];
+                a.Invoke(objectHit, 0f);
+                Debug.Log(objectHit);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Object/ClickLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickLogic : MonoBehaviour
+{
+    public float healthValue;
+    private float comboValue;
+    private float points;
+
+    //private float[] array3 = {DefaultCircle, BadCircle};
+
+    // gain point
+    public void DefaultCircle()
+    {
+        points++;
+        Debug.Log(points);
+    }
+
+    // Decrease life
+    public void BadCircle()
+    {
+        Debug.Log("points");
+    }
+
+    // Gain life
+
+    // increase point gain for an amount of time
+
+    // stop life drain for set period of time
+
+    // Gain point but add distractions
+
+}
