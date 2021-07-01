4fd202e01425a92fac91ac24efadff0644c7b767
i am jake <goldinghaml@stu.otc.school.nz>
2021-07-02 11:08:29 +1200

    timed circles
    this makes the circles appear at specific times

--- Assets/Object/Object_Placement.cs
+++ Assets/Object/Object_Placement.cs
@@ -10,10 +10,26 @@ public class Object_Placement : MonoBehaviour
     public GameObject Neutral;
     public GameObject Positive;
     public GameObject Starman;
+    Vector3[] NeutralPlacement = new[] {new Vector3(2f, 14f, 2f),
+                                        new Vector3(5f, 14f, 3f),
+                                        new Vector3(8f, 14f, 4f),
+                                        new Vector3(11f, 14f, 5f),
+                                        new Vector3(14f, 14f, 6f)};
+                                                        
+    //public float[] DistractionPlacementY = new float[] {1f,2f,3f};
+    private int i;
     
     // Update is called once per frame
-    void Start()
+    void Update()
     {
-        Instantiate(Distraction, new Vector3(0, 0, 0), Quaternion.identity).name = "Distraction"; // wtf how does this work
+        
+        try{
+        if(NeutralPlacement[i].z<=Time.time)
+        {
+            Instantiate(Neutral, new Vector3(NeutralPlacement[i].x, NeutralPlacement[i].y, 0f), Quaternion.identity).name = "Neutral"; // wtf how does this work
+            i++;
+        }}catch {}
+        
+
     }
 }
