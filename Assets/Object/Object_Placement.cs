696d3ddd7f352737f917674a8dc223d5c230f75f
i am jake <goldinghaml@stu.otc.school.nz>
2021-07-07 09:39:04 +1200

    added way to detect last object in array

--- Assets/Object/Object_Placement.cs
+++ Assets/Object/Object_Placement.cs
@@ -4,40 +4,43 @@ using UnityEngine;
 
 public class Object_Placement : MonoBehaviour
 {
+    
+    public GameObject[] circle = new GameObject[] {};
     public GameObject ComboBoost;
     public GameObject Distraction;
     public GameObject Negative;
     public GameObject Neutral;
     public GameObject Positive;
     public GameObject Starman;
-    public Vector3[] NeutralPlacement = new[] {new Vector3(2f, 14f, 5f),
-                                        new Vector3(3f, 14f, 5.5f),
-                                        new Vector3(4f, 14f, 6f),
-                                        new Vector3(3.5f, 14f, 6.5f),
-                                        new Vector3(6f, 14f, 7f),
-                                        new Vector3(7f, 14f, 7.5f)};
-    /*Vector3[] NeutralPlacement = new[] {new Vector3(2f, 14f, 5f),
-                                        new Vector3(3f, 14f, 5.5f),
-                                        new Vector3(4f, 14f, 6f),
-                                        new Vector3(3.5f, 14f, 6.5f),
-                                        new Vector3(6f, 14f, 7f),
-                                        new Vector3(7f, 14f, 7.5f)};*/
+
+    private int ComboBoostArrayPos;
+    private int DistractionArrayPos;
+    private int NegativeArrayPos;
+    private int NeutralArrayPos;
+    private int PositiveArrayPos;
+    private int StarmanArrayPos;
+
+    public Vector3[] ComboBoostPlacement = new Vector3[] {new Vector3(0f, 0f, 0f)};
+    public Vector3[] DistractionPlacement = new Vector3[] {new Vector3(0f, 0f, 0f)};
+    public Vector3[] NegativePlacement = new Vector3[] {new Vector3(0f, 0f, 0f)};
+    public Vector3[] NeutralPlacement = new Vector3[] {new Vector3(0f, 0f, 0f)};
+    public Vector3[] PositivePlacement = new Vector3[] {new Vector3(0f, 0f, 0f)};
+    public Vector3[] StarmanPlacement = new Vector3[] {new Vector3(0f, 0f, 0f)};
                                                         
     //public float[] DistractionPlacementY = new float[] {1f,2f,3f};
-    private int i;
     
-    // Update is called once per frame
     void Update()
     {
         
-        //try{
-        if(NeutralPlacement[i].z<=Time.time)
-        {
-            Instantiate(Neutral, new Vector3(NeutralPlacement[i].x, NeutralPlacement[i].y, i/10000f), Quaternion.identity).name = "Neutral";
-            i++;
-            print(NeutralPlacement.Size);
-
-        }//}catch {}
+        
+        if( NeutralPlacement.Length != NeutralArrayPos &&
+            NeutralPlacement[NeutralArrayPos].z<=Time.time) {
+            Instantiate(Neutral, new Vector3(   NeutralPlacement[NeutralArrayPos].x,
+                                                NeutralPlacement[NeutralArrayPos].y,
+                                                NeutralArrayPos/10000f),
+                                                Quaternion.identity).name = "Neutral";
+            NeutralArrayPos++;
+        }
         
 
     }
