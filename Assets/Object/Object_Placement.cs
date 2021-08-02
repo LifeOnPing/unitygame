9b41c92da3a1699928a72a411e72b61c5fcdd2c9
i am jake <goldinghaml@stu.otc.school.nz>
2021-08-02 15:19:04 +1200

    nothing much changed

--- Assets/Object/Object_Placement.cs
+++ Assets/Object/Object_Placement.cs
@@ -9,6 +9,7 @@ public class Object_Placement : MonoBehaviour
 
     public int[] ArrayPos;
 
+    [Header("Circle Placement")]
     public Vector3[] ComboBoostPlacement;
     public Vector3[] DistractionPlacement;
     public Vector3[] NegativePlacement;
@@ -16,7 +17,7 @@ public class Object_Placement : MonoBehaviour
     public Vector3[] PositivePlacement;
     public Vector3[] StarmanPlacement;
 
-    public Vector3[][] john = new Vector3[5][];
+    public Vector3[][] PlacementArray = new Vector3[6][];
     //Vector3 = new int[20][];
 
     private GameObject circle;
@@ -26,15 +27,19 @@ public class Object_Placement : MonoBehaviour
     {
         ArrayPos = new int[CircleTypes.Length];
 
-        john[0] = ComboBoostPlacement;
-        john[1] = DistractionPlacement;
+        PlacementArray[0] = ComboBoostPlacement;
+        PlacementArray[1] = DistractionPlacement;
+        PlacementArray[2] = NegativePlacement;
+        PlacementArray[3] = NeutralPlacement;
+        PlacementArray[4] = PositivePlacement;
+        PlacementArray[5] = StarmanPlacement;
     }
 
     void Update()
     {
-        print(john[1][5].x);   now make this work
-        /*while(NeutralPlacement.Length != Pos)
+        while(NeutralPlacement.Length != Pos)           look at arkive and fix this
         {
+        print("PlacementArray[1][5].x");
             if( NeutralPlacement.Length != ArrayPos[0] &&
                 NeutralPlacement[ArrayPos[0]].z<=Time.time) {
                 circle = (GameObject)Instantiate(CircleTypes[0], new Vector3(   NeutralPlacement[ArrayPos[0]].x,
@@ -46,7 +51,7 @@ public class Object_Placement : MonoBehaviour
                 ArrayPos[0]++;
             }
             Pos++;
-        }*/
+        }
         
 
     }
