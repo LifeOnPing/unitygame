20856436ae0990c463c40e7daf36d1725abdd7b6
i am jake <goldinghaml@stu.otc.school.nz>
2021-08-12 14:15:38 +1200

    made basic placement script

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,6 +7,14 @@ public class test : MonoBehaviour
 {
     public Object_Placement ObjectPlacement;
     public Vector3[] ObjectPlaceholder;
+    public float counter;
+    public float BeatsPerMinute;
+    private int i = 0;
+
+    void Start()
+    {
+        BeatsPerMinute = 60/BeatsPerMinute;
+    }
 
     void Update()
     {
@@ -14,10 +22,21 @@ public class test : MonoBehaviour
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             
-            
+            if(Input.GetAxis("Fire1") == 1 && i == 0) {
+                Vector3[] temp = new Vector3[ObjectPlaceholder.Length+1];
+                ObjectPlaceholder.CopyTo(temp, 0);
+                ObjectPlaceholder = temp;
+                print(ObjectPlaceholder.Length);
+                ObjectPlaceholder[ObjectPlaceholder.Length-1] = new Vector3(mousePos.x, mousePos.y, counter);
+                counter += BeatsPerMinute;
+                i = 1;
+                ObjectPlacement.ComboBoostPlacement = ObjectPlaceholder;
+            } else if(Input.GetAxis("Fire1") == 0 && i == 1){
+                i = 0;
+            }
 
-            ObjectPlaceholder.Add(mousePos.x, mousePos.y, ObjectPlaceholder.Length);        add to placement array
-            ObjectPlaceholder = ObjectPlaceholder.Select().ToArray();
+            //ObjectPlaceholder[len(ObjectPlaceholder)+1] = new Vector3(mousePos.x, mousePos.y, ObjectPlaceholder.Length);    //    add to placement array
+            //ObjectPlaceholder = ObjectPlaceholder.Select().ToArray();
             //ObjectPlacement.ComboBoostPlacement = ObjectPlaceholder;
         }
     }
